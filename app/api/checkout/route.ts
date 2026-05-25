da92dfafe5cc7113f81035e1f714038d1120c176
housniadmy-rgb <housniadmy@yahoo.de>
2026-05-25 11:42:45 +0200

    Dynamischer Stripe Import

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,10 +1,9 @@
 import { NextResponse } from "next/server"
-import Stripe from "stripe"
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "")
 
 export async function POST(request: Request) {
   try {
+    const Stripe = (await import("stripe")).default
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "")
     const { priceId } = await request.json()
     
     const session = await stripe.checkout.sessions.create({
