6f4b93e6de00ee3fcf4e8494578ad4a764e02b8f
housniadmy-rgb <housniadmy@yahoo.de>
2026-06-03 17:28:48 +0200

    Layout final

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,5 +12,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="de">
       <body className="bg-white text-gray-900 text-sm md:text-base lg:text-lg min-h-screen antialiased flex flex-col">
         <script dangerouslySetInnerHTML={{ __html: `(function(){var p=new URLSearchParams(window.location.search);var lang=p.get("setLang");if(lang){localStorage.setItem("lang",lang);var u=new URL(window.location);u.searchParams.delete("setLang");window.history.replaceState({},"",u.toString())}})()` }} />
-        <main className="flex-1">{children}</main>
+        <main className="flex-1">{children}</main>      </body>
+    </html>
+  )
+}
        
